74307452deeef441616d50691ffa51b964366b6a
Axrich <116589250+Axrich@users.noreply.github.com>
2023-05-28 22:31:13 +0500

    Update UpdatedApp.java

--- src/main/java/lang/print/gaps/task1/UpdatedApp.java
+++ src/main/java/lang/print/gaps/task1/UpdatedApp.java
@@ -1,6 +1,7 @@
 public class UpdatedApp {
     public static void main(String[] args) {
         System.out.println("I'm updated");
-        System.out.println("can be compiled. And can write to console");
+        System.out.println("can be compiled");
+        System.out.println("And can write to console");
     }
 }
