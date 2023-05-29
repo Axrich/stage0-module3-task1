f0e32aa29a6cb80185ba7c13dc056016f7e45c2c
Axrich <116589250+Axrich@users.noreply.github.com>
2023-05-29 17:52:16 +0500

    Update UpdatedApp.java

--- src/main/java/lang/print/gaps/task1/UpdatedApp.java
+++ src/main/java/lang/print/gaps/task1/UpdatedApp.java
@@ -1,3 +1,5 @@
+import org.junit.jupiter.api.Test;
+
 public class UpdatedApp {
     public static void main(String[] args) {
         System.out.println("I'm updated\ncan be compiled. And can write to console");
